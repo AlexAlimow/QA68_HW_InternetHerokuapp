8b05fe2d49928fe30c13cb7a469126c670905da8
AlexAlimow <gorlum007rus@gmail.com>
2025-10-15 21:33:49 +0200

    first commit

---
+++ src/main/java/com/internetHerokuapp/pages/HomePage.java
@@ -0,0 +1,15 @@
+package com.internetHerokuapp.pages;
+
+import com.internetHerokuapp.core.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+
+}
---
+++ src/test/java/com/internetHerokuapp/tests/HomePageTests.java
@@ -0,0 +1,11 @@
+package com.internetHerokuapp.tests;
+
+import com.internetHerokuapp.core.TestBase;
+import org.junit.jupiter.api.BeforeEach;
+
+public class HomePageTests extends TestBase {
+    @BeforeEach
+    public void precondition() {
+        //new HomePageTests(driver)
+    }
+}
